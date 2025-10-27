d51705affba5c47bad205e1836abbc3726662171
Your Name <you@example.com>
2025-10-27 11:09:33 +0200

    otrais commits

---
+++ ticket-price.js
@@ -0,0 +1,10 @@
+let visitorAge = 12;
+let isStudent = true;
+
+if (visitorAge <= 6) {
+  console.log("Ieeja bez maksas.");
+} else if (isStudent) {
+  console.log("Biļetes cena: 5 EUR.");
+} else {
+  console.log("Biļetes cena: 10 EUR.");
+}
